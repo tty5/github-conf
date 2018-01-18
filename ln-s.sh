41438a74e2207c168f8e75809c2c8d8efff33c03
tty5 <26674599+tty5@users.noreply.github.com>
2018-01-18 15:02:34 +0800

    Update ln-s.sh

--- ln-s.sh
+++ ln-s.sh
@@ -2,5 +2,6 @@ ln -s github-conf/.bash_aliases
 ln -s github-conf/.bundle.vim
 ln -s github-conf/.gitconfig
 ln -s github-conf/.vimrc
+ln -s github-conf/.gdbinit
 
 head -3 .bash_aliases |cut -c 2- >> ~/.bashrc
