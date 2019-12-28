fcfc2176a9e1ec916be001e1833187aacdd07c58
tty5 <26674599+tty5@users.noreply.github.com>
2019-12-28 11:10:04 +0800

    Update ustack.py

--- ustack.py
+++ ustack.py
@@ -2,10 +2,10 @@ import re
 import sys
 import json
 d={}
-a = re.sub('\+[\d]*', '', sys.stdin.read().replace('\n','')).split('@')
+a = re.sub('\+[\d]*', '', open(sys.argv[1]).read().replace('\n','')).split('@')
 a.pop(0)
 for item in a:
-    key, count = item.split(':')
-    d[key] = d[key] + int(count) if key in d else 0
+    key, count = item.rsplit(':', 1)
+    d[key] = d[key] + int(count) if key in d else int(count)
 
 print(json.dumps(d, indent=2))
