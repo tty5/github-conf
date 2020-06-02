bd2b78f0a014db37baa7781be6cb45ac6f7e002d
tty5 <26674599+tty5@users.noreply.github.com>
2020-06-02 12:01:32 +0800

    Update git-auto-commit.sh

--- git-auto-commit.sh
+++ git-auto-commit.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+#cmd /c ""C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\123\Desktop\note\git-auto-commit.sh"
+
+cd "$(dirname "$0")"
 
 if [[ -n "$(git status --porcelain)" ]]; then git add .; git commit -m auto; fi
 
 git fetch
 
-if [[ "$(git rev-parse HEAD)" == "$(git rev-parse origin/master)" ]];
+if [[ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]];
 then
     git rebase origin/master; git push origin HEAD:master
 fi
