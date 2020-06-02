a326ae8bf6ef8a3453dcc538790fb63140bfb574
tty5 <26674599+tty5@users.noreply.github.com>
2020-06-02 13:32:43 +0800

    Update git-auto-commit.sh

--- git-auto-commit.sh
+++ git-auto-commit.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-#cmd /c ""C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\123\Desktop\note\git-auto-commit.sh"
+# cmd /c ""C:\Program Files\Git\bin\bash.exe" --login -i -- C:\Users\123\Desktop\note\git-auto-commit.sh"
+
+# win10 click whether login to run it. imp
 
 cd "$(dirname "$0")"
 
@@ -12,3 +14,4 @@ if [[ "$(git rev-parse HEAD)" != "$(git rev-parse origin/master)" ]];
 then
     git rebase origin/master; git push origin HEAD:master
 fi
+
