741571a0f03de64fe29990554d595ef15483ab81
tty5 <26674599+tty5@users.noreply.github.com>
2020-10-28 10:16:55 +0800

    Update update-sock5.sh

--- nginx/v2r/update-sock5.sh
+++ nginx/v2r/update-sock5.sh
@@ -1,3 +1,4 @@
 cd "$(dirname "$0")"
-python3 vmess2json.py   --subscribe "$(cat /tmp/v2r.url)" -o /tmp/v2r.json && echo restart v2r && docker restart v2r
+python3 vmess2json.py --subscribe "$(cat /tmp/v2r.url)" -o /tmp/v2r-u.json &&
+echo restart v2r && cp /tmp/v2r-u.json /tmp/v2r.json &&  docker restart v2r
 
