be1dbb73898ba1015255f62122aeea11b8b2e453
tty5 <26674599+tty5@users.noreply.github.com>
2020-11-01 11:49:19 +0800

    Update update-sock5.sh

--- nginx/v2r/update-sock5.sh
+++ nginx/v2r/update-sock5.sh
@@ -1,6 +1,6 @@
 # docker run --name v2r --log-opt max-size=1m --user :1080 -v /tmp/v2r.json:/etc/v2ray/config.json --network host --restart always -d tty/v2r
 
-# docker logs v2r -f |grep -e 'taking detour' -e 'default route'
+# docker logs v2r -f |tee >(grep '\[direct\]' ) > >(GREP_COLOR='01;36' grep 'default route')
 
 cd "$(dirname "$0")"
 python3 vmess2json.py --subscribe "$(cat /tmp/v2r.url)" -o /tmp/v2r-u.json &&
