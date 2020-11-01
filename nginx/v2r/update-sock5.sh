c6307902655635517d12dfbeef861199c864c14d
tty5 <26674599+tty5@users.noreply.github.com>
2020-11-01 12:32:20 +0800

    Update update-sock5.sh

--- nginx/v2r/update-sock5.sh
+++ nginx/v2r/update-sock5.sh
@@ -1,8 +1,8 @@
-# docker run --name v2r --log-opt max-size=1m --user :1080 -v /tmp/v2r.json:/etc/v2ray/config.json --network host --restart always -d tty/v2r
+# docker run --name v2r --log-opt max-size=1m --user :1080 -v /root/v2r.json:/etc/v2ray/config.json --network host --restart always -d tty/v2r
 
 # docker logs v2r -f |tee >(grep '\[direct\]' ) > >(GREP_COLOR='01;36' grep 'default route')
 
 cd "$(dirname "$0")"
-python3 vmess2json.py --subscribe "$(cat /tmp/v2r.url)" -o /tmp/v2r-u.json &&
-echo restart v2r && cp /tmp/v2r-u.json /tmp/v2r.json &&  docker restart v2r
+python3 vmess2json.py --subscribe "$(cat /root/v2r.url)" -o /root/v2r-u.json &&
+echo restart v2r && cp /root/v2r-u.json /root/v2r.json &&  docker restart v2r
 
